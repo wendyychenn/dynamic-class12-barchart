788c6d91980362e9dd4ec4e0527418f0a8bae79c
Wendy Chen <wchen242@se14l0120-08.ad.bcit.ca>
2023-03-20 14:35:49 -0700

    adding files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chart.js 2
+https://react-chartjs-2.js.org/
+
+## Dependency Installation
+```
+npm i react-chartjs-2
+```
+
+# Bar Chart
---
+++ components/BarChart/index.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import styles from './BarChart.module.css'
+
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default function BarChart() {
+    
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [15323, 19213, 12124, 17423, 19213, 23214, 12142],
+                    borderColor: 'rgb(53,162,235)',
+                    backgroundColor: 'rgba(53,162,235,0.4)'
+                }
+            ]
+        })
+
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: "Daily Revenue"
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+    
+    return(
+        <>
+        <div className={styles.container}>
+            <Bar data={chartData} options={chartOptions}/>
+        </div>
+        </>
+    )
+}
